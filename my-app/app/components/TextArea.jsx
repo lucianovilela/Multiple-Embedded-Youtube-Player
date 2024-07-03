0e907f64962de4683f883ea7eb34f243bd947b4c
lucianovilela <lucianovilela@gmail.com>
2024-07-03 20:16:10 -0300

    wip

--- my-app/app/components/TextArea.jsx
+++ my-app/app/components/TextArea.jsx
@@ -1,3 +1,4 @@
+"use client";
 import { TextareaAutosize } from '@mui/material'
 import React from 'react'
 
--- my-app/app/db/index.js
+++ my-app/app/db/index.js
@@ -1,7 +1,7 @@
 "use client";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
 import { getFirestore, collection, getDocs, setDoc,addDoc, deleteDoc, doc } from 'firebase/firestore/lite';
 import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
@@ -21,7 +21,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
